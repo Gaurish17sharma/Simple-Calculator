a151870c1f3a99a3101a4ae6a9edbbc5ad7108cd
Gaurish17Sharma <gaurishsharma03@gmail.com>
2023-08-21 17:05:50 +0530

    Add SCRIPT.js

---
+++ SCRIPT.js
@@ -0,0 +1,24 @@
+const ACbutton =document.getElementById("AC")
+const plusminusbutton =document.getElementById("+/-")
+const percentbutton =document.getElementById("%")
+const slashbutton =document.getElementById("/")
+const sevenbutton =document.getElementById("7")
+const eightbutton =document.getElementById("8")
+const ninebutton =document.getElementById("9")
+const multiplybutton =document.getElementById("*")
+const fourbutton =document.getElementById("4")
+const fivebutton =document.getElementById("5")
+const sixbutton =document.getElementById("6")
+const minusbutton =document.getElementById("-")
+const onebutton =document.getElementById("1")
+const twobutton =document.getElementById("2")
+const threebutton =document.getElementById("3")
+const plusbutton =document.getElementById("+")
+const zerobutton =document.getElementById("0")
+const decimalbutton =document.getElementById("dot")
+const equaltobutton =document.getElementById("=")
+
+let displayvalue = 0;
+let firstnumber = null;
+let sign = null;
+let secondnumber = null;
